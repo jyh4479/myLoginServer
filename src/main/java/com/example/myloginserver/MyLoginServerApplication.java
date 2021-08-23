d789b307349ed99973d5285d725a4e91c8d1aef8
Jeong yong hoon <jyh.dev@gmail.com>
2021-08-23 17:29:46 +0900

    CREATE SPRING BOOT PROJECT

---
+++ src/main/java/com/example/myloginserver/MyLoginServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.myloginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLoginServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLoginServerApplication.class, args);
+    }
+
+}
