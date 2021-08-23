43ccde7e9d59dea9896b21e99a8199fc8dea2c36
Jeong yong hoon <jyh.dev@gmail.com>
2021-08-23 17:53:45 +0900

    INITIAL SETTING
    1. hello world controller
    
    2. 데베 사용에 대한 오류

--- src/main/java/com/example/myloginserver/MyLoginServerApplication.java
+++ src/main/java/com/example/myloginserver/MyLoginServerApplication.java
@@ -2,9 +2,18 @@ package com.example.myloginserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) //현재는 데베 사용안함
 public class MyLoginServerApplication {
+
+    @RequestMapping("/")
+    String TestController(){
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyLoginServerApplication.class, args);
